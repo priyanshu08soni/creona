b5709bc6d74598eaf85ebfaf6a88a8a38c066838
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-11-18 16:14:43 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,24 @@
+
+import { navLinks } from '@/constants'
+import { UserButton } from '@clerk/nextjs'
+import { LogIn } from 'lucide-react'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+const Home = async ({searchParams} : SearchParamProps) => {
+
+  return (
+    <>
+      <section className='home' >
+        <h1 className='home-heading'>
+          Make Your Creative Images with MegaMinds
+        </h1>
+      </section>
+      <section className='sm:mt-12' >
+      </section>
+    </>
+  )
+}
+
+export default Home
